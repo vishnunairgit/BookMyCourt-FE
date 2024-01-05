9a6adb7cb56ff944ae92b6f350266c02ebde8b1b
vishnuacnair <137478225+vishnuacnair@users.noreply.github.com>
2024-01-05 12:12:50 +0530

    bootstrap js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { store } from './ToolKit/Store';
 
 
